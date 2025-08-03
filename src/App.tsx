191a9a8a43bfa37063e77c6eb7ccad94fff85368
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2025-08-03 23:13:54 +0700

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -60,11 +60,31 @@ export default function App() {
         {/* End Navbar */}
         {/* Hero */}
         <div className="xl:flex font-rubik mt-20 xl:mt-40 xl:justify-between xl:items-center">
-          <div className="xl:hidden xl:w-1/2 lg:relative flex w-full justify-center">
+          <div className="xl:hidden xl:w-1/2 relative flex w-full justify-center">
+            <img
+              src={getImageUrl("lcd", "png")}
+              alt="icon"
+              className="w-20 absolute top-0 left-6 md:left-44"
+            />
+            <img
+              src={getImageUrl("code", "png")}
+              alt="icon"
+              className="w-20 absolute top-40 left-6 md:left-44"
+            />
+            <img
+              src={getImageUrl("avo", "png")}
+              alt="icon"
+              className="w-20 absolute top-0 right-6 md:right-44"
+            />
+            <img
+              src={getImageUrl("oscilloscope", "png")}
+              alt="icon"
+              className="w-20 absolute bottom-0 right-6 md:right-44"
+            />
             <motion.img
               src={getImageUrl("hero", "png")}
               alt="hero"
-              className="w-64 h-6w-64 rounded-full border-4 border-[#282825]"
+              className="w-64 h-6w-64 rounded-full border-4 border-[#282825] relative"
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
@@ -76,7 +96,9 @@ export default function App() {
           </div>
 
           <div className="w-full xl:w-1/2 max-lg:mt-10">
-            <p className="text-lg md:text-xl text-[#52514e] mb-4">Hello,</p>
+            <p className="text-lg md:text-xl text-[#52514e] tracking-wide mb-4">
+              Hello,
+            </p>
             <p className="text-4xl md:text-6xl font-medium text-[#282825] mb-4">
               I'm Ridwan Bahtiar.
             </p>
@@ -88,13 +110,33 @@ export default function App() {
               <div className="w-[8.9rem] h-[2.7rem] lg:w-[10.8rem] lg:h-[3.7rem] bg-[#282825] absolute top-[-0.8rem] left-2 rounded-xl"></div>
               <Link
                 to="#contact"
-                className="text-sm lg:text-base font-medium text-[#282825] py-2.5 px-6 lg:py-4 lg:px-8 bg-[#f7cb45] border-2 border-[#282825] rounded-xl absolute top-[-1.2rem] left-0 transition-all duration-300 lg:hover:top-[-1rem] lg:hover:left-[0.2rem] max-sm:active:top-[-1rem] max-sm:active:left-[0.1rem]"
+                className="text-sm lg:text-base font-medium text-[#282825] py-2.5 px-6 lg:py-4 lg:px-8 bg-[#f7cb45] border-2 border-[#282825] rounded-xl absolute top-[-1.2rem] left-0 transition-all duration-300 lg:active:top-[-1rem] lg:active:left-[0.2rem] max-sm:active:top-[-1rem] max-sm:active:left-[0.1rem]"
               >
                 Start Growing
               </Link>
             </div>
           </div>
           <div className="max-xl:hidden lg:w-1/2 lg:relative">
+            <img
+              src={getImageUrl("code", "png")}
+              alt="icon"
+              className="w-28 absolute top-0 left-24"
+            />
+            <img
+              src={getImageUrl("lcd", "png")}
+              alt="icon"
+              className="w-28 absolute bottom-28 left-24"
+            />
+            <img
+              src={getImageUrl("avo", "png")}
+              alt="icon"
+              className="w-28 absolute bottom-28 right-0"
+            />
+            <img
+              src={getImageUrl("oscilloscope", "png")}
+              alt="icon"
+              className="w-28 absolute top-20 right-[-2rem]"
+            />
             <div className="w-96 h-96 bg-[#282825] rounded-full absolute bottom-[-12rem] left-[11rem]"></div>
             <div className="absolute bottom-[-11rem] left-40">
               <motion.img
