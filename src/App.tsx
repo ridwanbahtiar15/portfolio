4c34aad0a9c2bb8ba08febb83d5ea81e92a6eb58
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2025-08-03 09:35:07 +0700

    fix some features

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,106 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import getImageUrl from "./utils/imageGetter";
 
 export default function App() {
+  const [isDropdownShow, setIsDropdownShow] = useState(true);
+
   return (
     // Hero
-    <div className="font-rubik w-full py-6 px-6 lg:py-12 lg:px-28 bg-[#f9f5f2] h-screen">
-      {/* Navbar */}
-      <div className="flex justify-between text-[#282825]">
-        <div className="max-sm:text-lg text-2xl font-medium">
-          Ridwan.Bahtiar
-        </div>
-        <div className="max-lg:hidden text-lg flex gap-16">
-          <p className="transition-all duration-100 hover:scale-110">About</p>
-          <p className="transition-all duration-100 hover:scale-110">
-            Projects
-          </p>
-          <p className="transition-all duration-100 hover:scale-110">Contact</p>
-        </div>
-      </div>
-      {/* End Navbar */}
-      {/* Hero */}
-      <div className="xl:flex font-rubik mt-20 xl:mt-40 xl:justify-between xl:items-center">
-        <div className="xl:hidden xl:w-1/2 lg:relative flex w-full justify-center">
-          <img
-            src={getImageUrl("hero", "png")}
-            alt="hero"
-            className="w-64 h-6w-64 rounded-full border-4 border-[#282825]"
-          />
-        </div>
-
-        <div className="w-full xl:w-1/2 max-lg:mt-10">
-          <p className="text-lg lg:text-xl text-[#52514e] mb-4">Hello,</p>
-          <p className="text-4xl lg:text-6xl font-medium text-[#282825] mb-4">
-            I'm Ridwan Bahtiar.
-          </p>
-          <p className="text-md lg:text-xl text-[#52514e] tracking-wide mb-12">
-            I'm passionate about programming and electronics, and I enjoy
-            building projects that combine both fields.
-          </p>
-          <div className="relative">
-            <div className="w-[8.9rem] h-[2.7rem] lg:w-[10.8rem] lg:h-[3.7rem] bg-[#282825] absolute top-[-0.8rem] left-2 rounded-xl"></div>
-            <Link
-              to="#contact"
-              className="text-sm lg:text-base font-medium text-[#282825] py-2.5 px-6 lg:py-4 lg:px-8 bg-[#f7cb45] border-2 border-[#282825] rounded-xl absolute top-[-1.2rem] left-0 transition-all duration-300 lg:hover:top-[-1rem] lg:hover:left-[0.2rem] max-sm:active:top-[-1rem] max-sm:active:left-[0.1rem]"
-            >
-              Start Growing
-            </Link>
+    <>
+      <div className="font-rubik w-full py-6 px-6 lg:py-12 lg:px-28 bg-[#f9f5f2] h-screen">
+        {/* Navbar */}
+        <div className="flex justify-between text-[#282825]">
+          <div className="max-sm:text-lg text-2xl font-medium">
+            Ridwan.Bahtiar
+          </div>
+          <div
+            className="lg:hidden"
+            onClick={() => {
+              setIsDropdownShow(true);
+              console.log("ok");
+            }}
+          >
+            <img src={getImageUrl("u_align-right", "svg")} alt="burger" />
+          </div>
+          <div className="max-lg:hidden text-lg flex gap-16">
+            <p className="transition-all duration-100 hover:scale-110">About</p>
+            <p className="transition-all duration-100 hover:scale-110">
+              Projects
+            </p>
+            <p className="transition-all duration-100 hover:scale-110">
+              Contact
+            </p>
           </div>
         </div>
-        <div className="max-xl:hidden lg:w-1/2 lg:relative">
-          <div className="w-96 h-96 bg-[#282825] rounded-full absolute bottom-[-12rem] left-[11rem]"></div>
-          <div className="absolute bottom-[-12rem] left-40">
+        {/* End Navbar */}
+        {/* Hero */}
+        <div className="xl:flex font-rubik mt-20 xl:mt-40 xl:justify-between xl:items-center">
+          <div className="xl:hidden xl:w-1/2 lg:relative flex w-full justify-center">
             <img
               src={getImageUrl("hero", "png")}
               alt="hero"
-              className="w-96 h-96 rounded-full border-4 border-[#282825]"
+              className="w-64 h-6w-64 rounded-full border-4 border-[#282825]"
             />
           </div>
+
+          <div className="w-full xl:w-1/2 max-lg:mt-10">
+            <p className="text-lg md:text-xl text-[#52514e] mb-4">Hello,</p>
+            <p className="text-4xl md:text-6xl font-medium text-[#282825] mb-4">
+              I'm Ridwan Bahtiar.
+            </p>
+            <p className="text-md md:text-xl text-[#52514e] tracking-wide mb-12">
+              I'm passionate about programming and electronics, and I enjoy
+              building projects that combine both fields.
+            </p>
+            <div className="relative">
+              <div className="w-[8.9rem] h-[2.7rem] lg:w-[10.8rem] lg:h-[3.7rem] bg-[#282825] absolute top-[-0.8rem] left-2 rounded-xl"></div>
+              <Link
+                to="#contact"
+                className="text-sm lg:text-base font-medium text-[#282825] py-2.5 px-6 lg:py-4 lg:px-8 bg-[#f7cb45] border-2 border-[#282825] rounded-xl absolute top-[-1.2rem] left-0 transition-all duration-300 lg:hover:top-[-1rem] lg:hover:left-[0.2rem] max-sm:active:top-[-1rem] max-sm:active:left-[0.1rem]"
+              >
+                Start Growing
+              </Link>
+            </div>
+          </div>
+          <div className="max-xl:hidden lg:w-1/2 lg:relative">
+            <div className="w-96 h-96 bg-[#282825] rounded-full absolute bottom-[-12rem] left-[11rem]"></div>
+            <div className="absolute bottom-[-12rem] left-40">
+              <img
+                src={getImageUrl("hero", "png")}
+                alt="hero"
+                className="w-96 h-96 rounded-full border-4 border-[#282825]"
+              />
+            </div>
+          </div>
         </div>
+        {/* End Hero */}
       </div>
-      {/* End Hero */}
-    </div>
+      {/* Navbar Mobile */}
+      {isDropdownShow && (
+        <div className="font-rubik w-full py-6 px-6 bg-[#f9f5f2]/80 backdrop-blur-md h-screen lg:hidden max-lg:absolute max-lg:top-0">
+          <div className="flex justify-between text-[#282825] backdrop-blur-none">
+            <div className="max-sm:text-lg text-2xl font-medium">
+              Ridwan.Bahtiar
+            </div>
+            <div className="lg:hidden" onClick={() => setIsDropdownShow(false)}>
+              <img src={getImageUrl("close", "svg")} alt="burger" />
+            </div>
+          </div>
+          <div className="lg:hidden text-lg text-[#282825] backdrop-blur-none mt-10 w-full grid grid-cols-3 gap-6">
+            <p className="transition-all duration-100 hover:scale-110 col-span-1 col-start-2 text-center">
+              About
+            </p>
+            <p className="transition-all duration-100 hover:scale-110 col-span-1 col-start-2 text-center">
+              Projects
+            </p>
+            <p className="transition-all duration-100 hover:scale-110 col-span-1 col-start-2 text-center">
+              Contact
+            </p>
+          </div>
+        </div>
+      )}
+      {/* End Navbar Mobile */}
+    </>
   );
 }
